298a8cf1e022c87d004f1897c12da36346ae8fc1
Mahamdul Hasan <mahamudulhasan.org@gmail.com>
2023-05-10 20:19:57 +0600

    project ready for car doctor client

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 5050;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Doctor is running!");
+});
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
