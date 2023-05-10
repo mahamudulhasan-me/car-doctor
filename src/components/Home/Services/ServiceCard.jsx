294757eaaedaa1a3a75361d2425e1ee84f78e298
Mahamdul Hasan <mahamudulhasan.org@gmail.com>
2023-05-10 20:10:32 +0600

    add server card

--- src/components/Home/Services/ServiceCard.jsx
+++ src/components/Home/Services/ServiceCard.jsx
@@ -2,17 +2,18 @@
 import React from "react";
 import { HiArrowRight } from "react-icons/hi";
 
-const ServiceCard = ({ image, title }) => {
+const ServiceCard = ({ service }) => {
+  const { title, img, price } = service;
   return (
     <div className="rounded-lg border p-4 overflow-hidden group">
       <img
-        src={image}
+        src={img}
         alt=""
         className="rounded-lg  group-hover:-translate-x-3 group-hover:-translate-y-3 transition-all duration-200"
       />
       <h2 className="font-semibold text-2xl my-2">{title}</h2>
       <div className="font-semibold flex justify-between text-lg items-center text-primary">
-        <p>Price : $20.00</p>
+        <p>Price : ${price}</p>
         <HiArrowRight />
       </div>
     </div>
--- src/components/Home/Services/Services.jsx
+++ src/components/Home/Services/Services.jsx
@@ -1,23 +1,22 @@
-import React from "react";
-import service1 from "../../../assets/images/services/1.jpg";
-import service2 from "../../../assets/images/services/2.jpg";
-import service3 from "../../../assets/images/services/3.jpg";
-import service4 from "../../../assets/images/services/4.jpg";
-import service5 from "../../../assets/images/services/5.jpg";
-import service6 from "../../../assets/images/services/6.jpg";
+import React, { useEffect, useState } from "react";
 import SectionTitle from "../../Shared/SectionTitle/SectionTitle";
 import ServiceCard from "./ServiceCard";
+
 const Services = () => {
+  const [services, setServices] = useState([]);
+
+  useEffect(() => {
+    fetch(`./serviceData.json`)
+      .then((response) => response.json())
+      .then((data) => setServices(data));
+  }, []);
   return (
     <div className="mt-40 mb-20">
       <SectionTitle section="Service" title="Our Service Area" />
       <div className="grid grid-cols-3 mt-10 gap-6">
-        <ServiceCard image={service1} title="Electrical System" />
-        <ServiceCard image={service2} title="Engine Diagnostics" />
-        <ServiceCard image={service3} title="Auto Car Repair" />
-        <ServiceCard image={service4} title="Electrical System" />
-        <ServiceCard image={service5} title="Engine Diagnostics" />
-        <ServiceCard image={service6} title="Auto Car Repair" />
+        {services.map((service) => (
+          <ServiceCard key={service._id} service={service} />
+        ))}
       </div>
       <div className="mx-auto w-52 mt-12">
         <button className="myOutline-btn ">More Service</button>
