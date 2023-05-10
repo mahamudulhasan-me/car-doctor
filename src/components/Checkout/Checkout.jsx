db033852de1b43ee51d14d646cbbed29e1970e4d
Mahamdul Hasan <mahamudulhasan.org@gmail.com>
2023-05-10 22:28:40 +0600

    add checkout banner

--- src/components/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import CheckoutBanner from "../Shared/CheckoutBanner/CheckoutBanner";
 
 const Checkout = () => {
-  return <div>This is checkout page</div>;
+  return (
+    <div>
+      This is checkout page
+      <CheckoutBanner title="Check Out" pathName="checkout" />
+    </div>
+  );
 };
 
 export default Checkout;
